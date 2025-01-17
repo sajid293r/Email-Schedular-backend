d36d7b72734ed11bee6ed1511ec6ee242a558f77
Bytecraft94 <mrizwan.bytecraft@gmail.com>
2025-01-18 00:44:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const sgMail = require('@sendgrid/mail');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid'); // Import UUID for unique identifiers
 
@@ -10,6 +11,7 @@ const app = express();
 const port = 5000;
 
 // Use environment variables for API keys
+sgMail.setApiKey(process.env.SEND_GRID_KEY);
 
 mongoose.connect('mongodb://localhost:27017/emailScheduler', {
     useNewUrlParser: true,
